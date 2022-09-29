0cd5203f433d51bd2663e6eb1aed1243966e2212
michelle <formedtechnologies@gmail.com>
2022-09-28 18:06:15 -0700

    9/28: Parts 2 and 3 of code are complete Missing: the standard error

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Property of forMED
+# prepared by: Michelle Rivera
 # Part 2: Data Analysis
 # openCV - computer vision
 # cv2 will not appear on download use openCV-python but commands are the same
@@ -23,8 +25,30 @@ import datastructs
 files = []
 b = []
 meanK = []
-filter = int(input("Remove Noise? 1- yes 0-no: "))
 
+# Not necessary to uncomment this section if you know your folders match in length
+'''
+trials = int(input("Sets of Trials Being Analyzed: "))
+tests = int(input("Test Runs Being Analyzed: "))
+input_string = (input("Enter pressure values separated by space: "))
+pressures = input_string.split(" ")
+for i in range(len(pressures)):
+    pressures[i]=int(pressures[i])
+print(pressures)
+# while the number of values in pressure array does not match tests incorrect
+while len(pressures) != tests:
+    print("Incorrect Array Length. Please try again.")
+    # we run this again so we dont have to break or re-run the code
+    input_string = (input("Enter pressure values separated by space: "))
+    pressures = input_string.split(" ")
+    # make string variables into int
+    for i in range(len(pressures)):
+        pressures[i] = int(pressures[i])
+# temporary print to ensure program is working
+print("Length of Pressures match number of Tests")
+'''
+print("")
+filter = int(input("Remove Noise? 1- yes 0-no: "))
 # control no light file
 print("Please select control file")
 control_file = filedialog.askopenfilenames()
@@ -81,144 +105,23 @@ for folder in os.scandir(path):
     print(meanK)
 # necessary for reshaping
 meanK_array = np.array(meanK)
-# later edit to .reshape(len(folders), len(files))
+# later edit to .reshape(trials, pressures)
 meanK_values = meanK_array.reshape(2, 5)
 print(meanK_values)
+avg_meanK_values = np.mean(meanK_values, axis=0)
 
-# averaging for the final list
-'''
+
 # graphing
-# 9/20/2022
+# 9/28/2022
 xaxis = [10, 15, 20, 25, 35]
-
-# for i in range(len(xaxis)):
-# for i in range(len(meanK)):
-plt.plot(xaxis, meanK, 'ro', label= 'Average MeanK')
-fitted_graph = np.polyfit(xaxis, meanK, 2)
+plt.plot(xaxis, avg_meanK_values, 'ro', label= 'Average MeanK')
+fitted_graph = np.polyfit(xaxis, avg_meanK_values, 2)
 plt.plot(xaxis, np.polyval(fitted_graph, xaxis), color = 'blue', label = 'polyfit') # mark w x
 # r2 value
-slope, intercept, r_value, p_value, std_err = stats.linregress(xaxis, meanK)
+slope, intercept, r_value, p_value, std_err = stats.linregress(xaxis, avg_meanK_values)
 r_squared = r_value**2
 print(r_squared)
 # plotting
 plt.legend()
 plt.text(13, 0.25, r_squared, horizontalalignment = 'right')
 plt.show()
-'''
-
-'''
-#start of code
-#user inputs
-trials = int(input("Sets of Trials Being Analyzed: "))
-tests = int(input("Test Runs Being Analyzed: "))
-
-
-#user input array for pressures
-#might be cleaner to makes lines 12-17 a def
-input_string = (input("Enter pressure values separated by space: "))
-pressures = input_string.split(" ")
-#make string variables into int
-for i in range(len(pressures)):
-    pressures[i]=int(pressures[i])
-print(pressures)
-
-#while the number of values in pressure array does not match tests incorrect
-while len(pressures) != tests:
-    print("Incorrect Array Length. Please try again.")
-    # we run this again so we dont have to break or re-run the code
-    input_string = (input("Enter pressure values separated by space: "))
-    pressures = input_string.split(" ")
-    # make string variables into int
-    for i in range(len(pressures)):
-        pressures[i] = int(pressures[i])
-#temporary print to ensure program is working
-print("Length of Pressures match number of Tests")
-
-
-# values needed to be stored
-import ipdb
-# ipdb.set_trace()
-meanK = []
-# open directory
-filter_noise = 1
-testing_files = []
-
-control_file = filedialog.askopenfilenames()
-for i in range(len(control_file)):
-    control_dataset = Image.open(control_file[i])
-    h, w = np.shape(control_dataset)
-    contarray = np.zeros((h, w, control_dataset.n_frames))
-    for i in range(control_dataset.n_frames):
-        control_dataset.seek(i)
-        contarray[:, :, i] = np.array(control_dataset)
-    # 3d array: used for the subtraction
-    expim1 = contarray.astype(np.double)
-
-    # prints the pixels and the number of slices
-    # print(expim1.shape)
-    # convert to 1d array for each
-    # no_light = expim1.flatten(order='C')
-    # print so we can check
-    # print(no_light)
-
-while filter_noise == 1:
-    selected_files = filedialog.askopenfilenames()
-    testing_files = testing_files + list(selected_files)
-    filter_noise = int(input("Enter 1 to input enter 0 to continue: "))
-
-# now that we have the files and their respective directory find a way to iterate through them
-# step one just see if you can read them: accomplished
-# step two see if you can extract the data: accomplished
-# step three meanK values for more than one file: accomplished
-
-filter_process = int(input("To subtract control (1) to not (0): "))
-
-for i in range(len(testing_files)):
-    print(testing_files[i])
-    dataset = Image.open(testing_files[i])
-    # do the 3d array for each
-    h, w = np.shape(dataset)
-    tiffarray = np.zeros((h, w, dataset.n_frames))
-    for i in range(dataset.n_frames):
-            dataset.seek(i)
-            tiffarray[:, :, i] = np.array(dataset)
-    # 3d array
-    expim = tiffarray.astype(np.double)
-    # prints the pixels and the number of slices
-    print(expim.shape)
-    # convert to 1d array for each
-    b = expim.flatten(order='C')
-    # print so we can check
-    print(b)
-    # subtract the control no light from the other selected files: if filtering for noise
-    if filter_process == 1:
-        filter_1 = np.subtract(expim, expim1)
-        # print(filter_1)
-        flat_filter = filter_1.flatten(order = 'C')
-        meanK_formula = np.std(flat_filter)/np.mean(flat_filter)
-        meanK.append(meanK_formula)
-    else:
-        meanK_formula = np.std(b) / np.mean(b)
-        # print(meanK_formula)
-        meanK.append(meanK_formula)
-
-print(meanK)
-
-# part 3
-# graphing
-xaxis = [10, 15, 20, 25, 35]
-
-# for i in range(len(xaxis)):
-   # for i in range(len(meanK)):
-plt.plot(xaxis, meanK, 'ro', label= 'Average MeanK')
-fitted_graph = np.polyfit(xaxis, meanK, 2)
-plt.plot(xaxis, np.polyval(fitted_graph, xaxis), color = 'blue', label = 'polyfit') # mark w x
-#r2 value
-slope, intercept, r_value, p_value, std_err = stats.linregress(xaxis, meanK)
-r_squared = r_value**2
-print(r_squared)
-# plotting
-plt.legend()
-plt.text(13, 0.25, r_squared, horizontalalignment = 'right')
-plt.show()
-'''
